96e18b0b2dbf15de6e11f55c281e074163a1e105
DJNing <dengjn28@gmai.com>
2022-08-13 01:55:58 +0100

    Removed is_radar option in vod eval

--- pcdet/datasets/kitti/vod_official_eval/kitti_official_evaluate.py
+++ pcdet/datasets/kitti/vod_official_eval/kitti_official_evaluate.py
@@ -688,22 +688,13 @@ def get_official_eval_result(gt_annotations, dt_annotations, current_classes, pr
     elif custom_method == 3:
         print("Evaluating kitti by ROI")
 
-    if is_radar:
-        # Original OpenPCDet code
-        overlap_0_7 = np.array([[0.7, 0.5, 0.5, 0.7, 0.5, 0.7],
-                                [0.7, 0.5, 0.5, 0.7, 0.5, 0.7],
-                                [0.7, 0.5, 0.5, 0.7, 0.5, 0.7]])
-        overlap_0_5 = np.array([[0.7, 0.50, 0.50, 0.7, 0.50, 0.5],  # image
-                                [0.5, 0.25, 0.25, 0.5, 0.25, 0.5],  # bev
-                                [0.5, 0.25, 0.25, 0.5, 0.25, 0.5]])  # 3d
-    else:
-        overlap_0_7 = np.array([[0.75, 0.5, 0.5, 0.75, 0.5, 0.5],
-                                [0.75, 0.5, 0.5, 0.75, 0.5, 0.5],
-                                [0.75, 0.5, 0.5, 0.7, 0.5, 0.7]])
-        overlap_0_5 = np.array([[0.75, 0.5, 0.5, 0.75, 0.5, 0.5],
-                                [0.75, 0.5, 0.5, 0.75, 0.5, 0.5],
-                                [0.75, 0.5, 0.5, 0.75, 0.5, 0.5]])
-
+    # Original OpenPCDet code
+    overlap_0_7 = np.array([[0.7, 0.5, 0.5, 0.7, 0.5, 0.7],
+                            [0.7, 0.5, 0.5, 0.7, 0.5, 0.7],
+                            [0.7, 0.5, 0.5, 0.7, 0.5, 0.7]])
+    overlap_0_5 = np.array([[0.7, 0.50, 0.50, 0.7, 0.50, 0.5],  # image
+                            [0.5, 0.25, 0.25, 0.5, 0.25, 0.5],  # bev
+                            [0.5, 0.25, 0.25, 0.5, 0.25, 0.5]])  # 3d
     # class:  0,    1,    2,   3,   4,    5
     min_overlaps = np.stack([overlap_0_7, overlap_0_5], axis=0)  # [2, 3, 6] = [num_overlaps, metrics, classes]
 
